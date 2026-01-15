9fbd7e072db4ee762cc570083fe0388eac7deb93
Abdulla <kaldybekabdulaziz@gmail.com>
2026-01-15 14:07:50 +0100

    Add files via upload

---
+++ First-Project-Java-Vistula/Project_Task/FirstRestAPI/src/main/java/pl/edu/vistula/firstrestapi/shared/api/response/ErrorMessageResponse.java
@@ -0,0 +1,12 @@
+package pl.edu.vistula.firstrestapi.shared.api.response;
+
+public class ErrorMessageResponse {
+    private final String message;
+
+    public ErrorMessageResponse(String message){
+        this.message = message;
+    }
+    public String getMessage(){
+        return message;
+    }
+}
